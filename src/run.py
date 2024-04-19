7d6fb3aa84d6cd47ccdb020fd9818702059c5873
Denys-Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2024-04-19 19:28:39 +0300

    chore: use poetry to manage dependencies

--- src/run.py
+++ src/run.py
@@ -1,20 +1,33 @@
 import logging
+from dotenv import load_dotenv
 import uvicorn
 import ssl
-from dotenv import load_dotenv
 from os import environ
 
-if __name__ == "__main__":
+def run () -> None:
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     load_dotenv()
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=int(environ["PORT"]),
-        ssl_version=ssl.PROTOCOL_TLS,
-        ssl_keyfile=environ["SSL_KEYFILE"],
-        ssl_certfile=environ["SSL_CERTFILE"],
-        log_level="info",
-        reload=True
-    )
+
+    uvicorn_config = {
+        "app": "src.main:app",
+        "host": "0.0.0.0",
+        "port": int(environ["PORT"]),
+        "log_level": "info",
+        "reload": True
+    }
+
+    ssl_keyfile = environ.get("SSL_KEYFILE")
+    ssl_certfile = environ.get("SSL_CERTFILE")
+
+    if ssl_keyfile and ssl_certfile:
+        uvicorn_config.update({
+            "ssl_version": ssl.PROTOCOL_TLS,
+            "ssl_keyfile": ssl_keyfile,
+            "ssl_certfile": ssl_certfile
+        })
+
+    uvicorn.run(**uvicorn_config)
+
+if __name__ == "__main__":
+    run()
